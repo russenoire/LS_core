47164db3e619a76aeec1cf2e8522a456a7b09077
russenoire <demivisage@gmail.com>
2019-04-27 17:18:02 -0700

    Solve further exploration of Stringy Strings

--- exercises/lesson_1/easy_1/007_stringy_strings.rb
+++ exercises/lesson_1/easy_1/007_stringy_strings.rb
@@ -16,3 +16,20 @@ puts stringy(8) == '10101010'
 puts stringy(11) == '10101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
+
+# further exploration
+def stringy_extra(int, start = 1)
+  out_str = ''
+  if start == 0
+    (int / 2).times { out_str << '01' }
+    return int.odd? ? out_str << '0' : out_str
+  else
+    (int / 2).times { out_str << '10' }
+    return int.odd? ? out_str << '1' : out_str
+  end
+end
+
+puts stringy_extra(8, 0) == '01010101'
+puts stringy_extra(11, 0) == '01010101010'
+puts stringy_extra(4) == '1010'
+puts stringy_extra(7) == '1010101'
