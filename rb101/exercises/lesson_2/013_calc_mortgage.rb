4a61f486c736679192850f0539b20f1edd5b47e1
russenoire <demivisage@gmail.com>
2019-05-04 20:12:52 -0700

    Add loop logic for mortgage calc

--- rb101/exercises/lesson_2/013_calc_mortgage.rb
+++ rb101/exercises/lesson_2/013_calc_mortgage.rb
@@ -21,19 +21,64 @@ def loan_in_months()
 end
 
 def valid_number?(str)
-  # is it floaty? returns boolean
-
+  # is it floaty or integery? returns boolean
+  str.delete('%')
+  !!(/^\d+(\.+\d+)?$/.match(str))
 end
 
 def prompt(str)
   puts str
 end
 
+prompt("Welcome to your handy dandy Mortgage Calc!")
 
-welcome = <<-MSG
-  Welcome to your handy dandy Mortgage Calc!
-  Please enter the amount of your loan, in dollars and cents:
-MSG
+loop do
+  loan_amount = ''
+  loop do
+    prompt("Please enter the amount of your loan:")
+    loan_amount = gets.chomp
+    if valid_number?(loan_amount)
+      loan_amount = loan_amount.to_f
+      break
+    else
+      prompt("I can't work with that. Try again?")
+    end
+  end
 
-prompt(welcome)
-loan_amount = gets.chomp
+  apr = ''
+  loop do
+    prompt("Please enter your loan's APR, in percent (25%):")
+    apr = gets.chomp
+    if valid_number?(apr)
+      apr = apr.to_f
+      break
+    else
+      prompt("I can't work with that. Try again?")
+    end
+  end
+
+  loan_term_mo = ''
+  loop do
+    prompt("What is the duration of your loan in months?")
+    loan_term_mo = gets.chomp
+    if valid_number?(loan_term_mo)
+      loan_term_mo = loan_term_mo.to_i
+      break
+    else
+      prompt("I can't work with that. Try again?")
+    end
+  end
+
+  result = <<-RESULT
+  Given the loan amount $#{}:
+  with a term of #{} months,
+  at an annual percentage rate of #{}%,
+  your monthly payment is $#{}.
+  RESULT
+  prompt(result)
+
+  prompt("Would you like me to perform another calculation? [Y/N]")
+  answer = gets.chomp.downcase
+  break unless answer.start_with?('y')
+end
+prompt("This has been your handy dandy Mortgage Calc! Signing off.")
