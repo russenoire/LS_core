4c295daa970acfb03204ad8a0d34388c38518a61
russenoire <demivisage@gmail.com>
2019-05-09 15:11:10 -0700

    Complete first working version of Rock Paper Scissors exercise

--- rb101/exercises/lesson_2/014_rps_walkthru.rb
+++ rb101/exercises/lesson_2/014_rps_walkthru.rb
@@ -20,20 +20,28 @@ def who_won?(choice, computer_choice)
   answer
 end
 
-choice = ''
 loop do
-  prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-  choice = Kernel.gets().chomp().strip()
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp().strip()
 
-  if VALID_CHOICES.include?(choice)
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That choice is not valid. Try again?")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("You chose: #{choice}. Computer chose: #{computer_choice}.")
+  prompt("#{who_won?(choice, computer_choice)}".upcase)
+  prompt("Play again? [Y/n]")
+  play_again = Kernel.gets().chomp()
+
+  unless play_again.downcase.start_with?('y')
+    prompt("Thanks for playing! 'Bye.")
     break
-  else
-    prompt("That choice is not valid. Try again?")
   end
 end
-
-computer_choice = VALID_CHOICES.sample
-
-prompt("You chose: #{choice}. Computer chose: #{computer_choice}.")
-prompt("#{who_won?(choice, computer_choice)}".upcase)
-prompt("Play again? [Y/N]")
