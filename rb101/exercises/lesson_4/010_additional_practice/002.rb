1ec8f16a807eaabcf631d3300549e33cace42940
Anastassia Davis <demivisage@gmail.com>
2019-08-23 21:17:00 -0700

    Complete small problems, ex 1, easy soln

--- rb101/exercises/lesson_4/010_additional_practice/002.rb
+++
@@ -1,8 +0,0 @@
-ages = {"Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237}
-
-total_age = 0
-ages.each do |_, age|
-  total_age += age
-end
-
-p total_age
---
+++ rb101/exercises/lesson_4/easy_2/001_teddy_age.rb
@@ -0,0 +1,8 @@
+# random age generator/printer
+# inputs: none
+# outputs: a string containing teddy's age
+# model:
+# use random number generator with range, interpolate in string
+
+age = rand(20...200)
+puts "Teddy is #{age} years old!"
