a2eb410f8127fb8647e2394e3f0a81f8e41c0f0a
Anastassia Davis <demivisage@gmail.com>
2019-08-23 22:20:04 -0700

    Add harder soln for easy2 ex 2

--- rb101/exercises/lesson_4/easy_2/002_how_big_room.rb
+++ rb101/exercises/lesson_4/easy_2/002_how_big_room.rb
@@ -4,14 +4,32 @@
 # output
 # area in sq ft and sq m
 
-SQ_M_TO_SQ_FT = 10.7639
+# easy soln
+# SQ_M_TO_SQ_FT = 10.7639
+#
+# puts "Enter the length of the room in meters:"
+# length = gets.chomp.to_f
+# puts "Enter the width of the room in meters:"
+# width = gets.chomp.to_f
+#
+# area = length * width
+# area_imperial = area * SQ_M_TO_SQ_FT
+# puts "The area of the room is %0.2f square meters" % area + \
+#      " (%0.2f square feet)." % area_imperial
 
-puts "Enter the length of the room in meters:"
-length = gets.chomp.to_f
-puts "Enter the width of the room in meters:"
-width = gets.chomp.to_f
+# further exploration
+SQ_FT_TO_SQ_IN = 144
+SQ_FT_TO_SQ_CM = 929.0304
+
+puts "Enter the length of the room in feet:"
+length = gets.to_f
+puts "Enter the width of the room in feet:"
+width = gets.to_f
 
 area = length * width
-area_imperial = area * SQ_M_TO_SQ_FT
-puts "The area of the room is %0.2f square meters" % area + \
-     " (%0.2f square feet)." % area_imperial
+area_sq_in = area * SQ_FT_TO_SQ_IN
+area_sq_cm = area * SQ_FT_TO_SQ_CM
+
+puts "The area of the room is %0.2f square feet " % area + \
+     "(%0.2f square inches," % area_sq_in + \
+     " or %0.2f square centimeters)." % area_sq_cm
