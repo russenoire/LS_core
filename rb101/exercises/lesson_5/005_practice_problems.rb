acee1f217c7de4796a544b2349748463e1770fb1
Anastassia Davis <demivisage@gmail.com>
2019-11-24 16:40:53 -0800

    Lesson 5 practice problems in progress

--- rb101/exercises/lesson_5/005_practice_problems.rb
+++ rb101/exercises/lesson_5/005_practice_problems.rb
@@ -152,26 +152,19 @@ p new_arr
 # Given the following data structure and without modifying the original array,
 # use the map method to return a new array identical in structure to the
 # original but where the value of each integer is incremented by 1.
-# first try
 arr = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}]
 new_arr = arr.map do |subhsh|
-  subhsh.transform_values{|val| val += 1}
+  new_hsh = {}
+  subhsh.each{|key, val| new_hsh[key] = val + 1}
+  new_hsh
 end
 p new_arr
-
-# second try
-[{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}].map do |hsh|
-  increment_hash = {}
-  hsh.each {|key, val| increment_hash[key] = val + 1}
-  increment_hash
-end
 
 # Practice Problem 11
 # Given the following data structure use a combination of methods, including
 # either the select or reject method, to return a new array identical in
 # structure to the original but containing only the integers that are multiples
 # of 3.
-
 arr = [[2], [3, 5, 7], [9], [11, 13, 15]]
 
 arr.map do |subarr|
@@ -181,3 +174,94 @@ arr.map do |subarr|
 end
 
 p arr
+
+# Practice Problem 12
+# Given the following data structure, and without using the Array#to_h method,
+# write some code that will return a hash where the key is the first item in
+# each sub array and the value is the second item.
+arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
+hsh = {}
+
+arr.each do |subarr|
+  hsh[subarr[0]] = subarr[1]
+end
+p hsh
+
+# Practice Problem 13
+# Given the following data structure, return a new array containing the same
+# sub-arrays as the original but ordered logically by only taking into
+# consideration the odd numbers they contain.
+arr = [[1, 6, 7], [1, 4, 9], [1, 8, 3]]
+
+# Practice Problem 14
+# Given this data structure write some code to return an array containing the
+# colors of the fruits and the sizes of the vegetables. The sizes should be
+# uppercase and the colors should be capitalized.
+hsh = {
+  'grape' => {type: 'fruit', colors: ['red', 'green'], size: 'small'},
+  'carrot' => {type: 'vegetable', colors: ['orange'], size: 'medium'},
+  'apple' => {type: 'fruit', colors: ['red', 'green'], size: 'medium'},
+  'apricot' => {type: 'fruit', colors: ['orange'], size: 'medium'},
+  'marrow' => {type: 'vegetable', colors: ['green'], size: 'large'},
+}
+arr = []
+
+hsh.each_value do |subhsh|
+  if subhsh[:type] == 'fruit'
+    arr << subhsh[:colors].map { |color| color.capitalize }
+  else
+    arr << subhsh[:size].upcase
+  end
+end
+p arr
+
+# Practice Problem 15
+# Given this data structure write some code to return an array which contains
+# only the hashes where all the integers are even.
+arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
+
+arr.select do |hsh|
+  hsh.values.each do |ary|
+    ary.all?
+  end
+end
+p arr
+
+# Practice Problem 16
+# A UUID is a type of identifier often used as a way to uniquely identify
+# items...which may not all be created by the same system. That is, without any
+# form of synchronization, two or more separate computer systems can create new
+# items and label them with a UUID with no significant chance of stepping on
+# each other's toes.
+#
+# It accomplishes this feat through massive randomization. The number of
+# possible UUID values is approximately 3.4 X 10E38.
+#
+# Each UUID consists of 32 hexadecimal characters, and is typically broken into
+# 5 sections like this 8-4-4-4-12 and represented as a string.
+#
+# It looks like this: "f65c57f6-a6aa-17a8-faa1-a67f2dc9fa91"
+#
+# Write a method that returns one UUID when called with no parameters.
+# input: none / output: 36-char string in hexadecimal
+# define var str
+# str << '0123456789abcdef'to_a.sample
+# 36.times do
+# check length of str: if [8, 13, 18, 23].include?(str.length) << '-'
+# return str
+def make_UUID
+  uuid = ''
+  uuid_length = 36
+  dash_pos = [8,13,18,23]
+  hex_digits = '0123456789abcdef'
+
+  uuid_length.times do |iter|
+    if dash_pos.include?(iter)
+      uuid += '-'
+    else
+      uuid += hex_digits.each_char.to_a.sample
+    end
+  end
+  return uuid
+end
+p make_UUID
